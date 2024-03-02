316bc24d526c87462c8feea2c969434dd2f4e674
Jaehun <qkdwogns98@gmail.com>
2024-03-02 14:21:02 +0900

    chore: Add @emotion/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { css } from "@emotion/react";
+
 function App() {
-  return <>App</>;
+  return (
+    <div
+      css={css`
+        width: 100px;
+        height: 100px;
+        border: 1px solid gray;
+      `}
+    >
+      App
+    </div>
+  );
 }
 
 export default App;
